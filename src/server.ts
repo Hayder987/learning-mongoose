e7900ef9d1419ca4bef0d7286d3fde7eca6756dc
hayder <hayderbd4290@gmail.com>
2025-06-14 19:21:30 +0600

    setup and connected to mongodb atlas

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { Server } from 'http';
+import app from './app';
+import mongoose from 'mongoose';
+
+const PORT = 5000;
+
+let server: Server;
+
+async function main (){
+    try {
+        await mongoose.connect(`mongodb+srv://mongooseAdmin:hayder4290@cluster0.jes9ndo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log("connected with mongodb with mongoose")
+        
+        server = app.listen(PORT, ()=>{
+            console.log(`Server Running At port: ${PORT}`)
+        })
+        
+    } catch (error) {
+       console.log(error)  
+    }
+}
+
+main();
+
